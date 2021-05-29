58e805c8a20321abbe5cb0c77cf65f129f31e454
tuolomee <kejmarova@gmail.com>
2021-05-29 14:21:42 +0200

    Úkol č.14

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => {
+  const [likeUp, setLikeUp] = useState(0)
+  const [likeDown, setLikeDown] = useState(0)
+
+return (
+  <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button onClick={() => setLikeUp(likeUp+1)} id="btn-up" className="btn-like btn-like--up"></button>
+          <span id="likes-up" className="likes-count likes-count--up">{likeUp}</span>
+          <button onClick={() => setLikeDown(likeDown+1)} id="btn-down" className="btn-like btn-like--down"></button>
+          <span id="likes-down" className="likes-count likes-count--down">{likeDown}</span>
+        </div>
+      </div>
+    </div>
+);}
+
+render(<App />, document.querySelector('#app'));
